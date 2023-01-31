7224bffe8821a59c6bb18cd0a982ad1f29344e2f
mcanteros2021 <alum.mcanteros.2021@iesalixar.org>
2023-01-31 11:07:20 +0100

    Esquema pedidos

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 require('dotenv').config();
-
-var mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true
-  })
-  .then(() => console.log('connection successful'))
-  .catch((err) => console.error(err));
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var mongoose = require('mongoose');
+var pedidos = require('./routes/pedidosRoutes')
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Conexión //
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb+srv://Mario:1234@shamedb.d9vcgir.mongodb.net/shameDb?retryWrites=true&w=majority').then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((error) => console.error(error));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// MiddleWares
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// Rutas
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("/pedidos", pedidos );
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+// Server Listenning //
+
+app.listen(9000,()=>
+  console.log("Servidor escuchando en el puerto 9000")
+)
