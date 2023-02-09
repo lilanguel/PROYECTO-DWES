edec7e441acb7bccb1eda6037addae8f8b124703
ManuelJesusTG <90841523+ManuelJesusTG@users.noreply.github.com>
2023-02-09 20:53:30 +0100

    Ruta resenia

--- routes/reseniasRoutes.js
+++ routes/reseniasRoutes.js
@@ -2,13 +2,29 @@ const express = require("express");
 const reseniaSchema = require("../models/resenias.js");
 const router = express.Router();
 
-// create resenia
+/// POST de una nueva reseña
+router.post('/', function (req, res, next) {
+    reseniaSchema.create(req.body, function (err, reseniainfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
+});
 
-router.post("/resenias", (req, res) => {
-    const resenia = reseniaSchema(req.body);
-    resenia.save().then((data) => res.json(data)).catch((error) => res.json({
-        message: error
-    }));
+// PUT de una resea existente identificado por su Id
+router.put('/:id', function (req, res, next) {
+    reseniaSchema.findByIdAndUpdate(req.params.id, req.body, function (err,
+        reseniainfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
+});
+
+// DELETE de una reseña existente identificado por su Id
+router.delete('/:id', function (req, res, next) {
+    reseniaSchema.findByIdAndDelete(req.params.id, function (err, reseniainfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
 });
 
 // get all resenias
