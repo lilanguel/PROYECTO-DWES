ed4cf6515255444d94cb410a006ffc6e00efa1ca
ManuelJesusTG <90841523+ManuelJesusTG@users.noreply.github.com>
2023-02-10 18:03:08 +0100

    Get de resenia por ID

--- routes/reseniasRoutes.js
+++ routes/reseniasRoutes.js
@@ -4,16 +4,16 @@ const router = express.Router();
 
 /// POST de una nueva reseña
 router.post('/', function (req, res, next) {
-    reseniaSchema.create(req.body, function (err, reseniainfo) {
+    reseniaSchema.create(req.body, function (err, userinfo) {
         if (err) res.status(500).send(err);
         else res.sendStatus(200);
     });
 });
 
-// PUT de una resea existente identificado por su Id
+// PUT de una reseña existente identificado por su Id
 router.put('/:id', function (req, res, next) {
     reseniaSchema.findByIdAndUpdate(req.params.id, req.body, function (err,
-        reseniainfo) {
+        userinfo) {
         if (err) res.status(500).send(err);
         else res.sendStatus(200);
     });
@@ -21,16 +21,25 @@ router.put('/:id', function (req, res, next) {
 
 // DELETE de una reseña existente identificado por su Id
 router.delete('/:id', function (req, res, next) {
-    reseniaSchema.findByIdAndDelete(req.params.id, function (err, reseniainfo) {
+    reseniaSchema.findByIdAndDelete(req.params.id, function (err, userinfo) {
         if (err) res.status(500).send(err);
         else res.sendStatus(200);
     });
 });
 
-// get all resenias
+// get all reseñas
 router.get("/resenias", (req, res) => {
     reseniaSchema
         .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({
+            message: error
+        }));
+});
+
+// GET de reseña dependiendo de su ID
+router.get("/:id", (req, res) => {
+    reseniaSchema.findById(req.params.id)
         .then((data) => res.json(data))
         .catch((error) => res.json({
             message: error
